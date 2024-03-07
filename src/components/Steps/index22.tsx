78a0058c896857d8f417f57bba4bc33d1eea8e47
Andrii Stratiichuk <dazzlingerino@gmail.com>
2024-03-07 18:04:54 +0200

    Add pre-commit and pre-push hooks

---
+++ src/components/Steps/index22.tsx
@@ -0,0 +1,27 @@
+import type { FC } from 'react';
+import Step from 'src/components/common/Step';
+import styles from './Steps.module.css';
+
+export interface StepsProps {
+  prizes: number[];
+  currentStep: number;
+}
+
+const Steps: FC<StepsProps> = ({ prizes, currentStep }) => {
+  return (
+    <div className={styles.stepsContainer}>
+      {prizes.map((prize, index) => {
+        const stepIndex = prizes.length - 1 - index;
+
+        const isCompleted = stepIndex < currentStep;
+        const isActive = stepIndex === currentStep;
+
+        return (
+          <Step key={prize} stepNumber={prize} isCompleted={isCompleted} isActive={isActive} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Steps;
